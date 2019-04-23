ce3407d2bfc60d93ff1299e7c85e370cc470c859
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-04-22 19:33:23 -0700

    shard dataset util

--- pytorch/shard_dataset.py
+++ pytorch/shard_dataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # shard dataset for transformer-xl codebase
-# TODO(y): smarrer sharding to avoid breaking within article?
+# TODO(y): smarrer sharding to avoid breaking within articles?
 
 import argparse
 import math
@@ -8,39 +8,43 @@ import os
 import sys
 
 parser = argparse.ArgumentParser(description='shard utility')
-parser.add_argument('--target', type=str,
-                    default='/ncluster/data/transformer-xl-data/wikitext-103/train.txt',
+parser.add_argument('--datadir', type=str,
+                    default='/ncluster/data/transformer-xl-data/wikitext-103',
                     help='location of train.txt')
 parser.add_argument('--shards', type=int, default=4, help='how many ways to shard')
 args = parser.parse_args()
 
 
-def main():
-    assert os.path.exists(args.target), args.target
-    assert args.target.endswith('train.txt')
+def shard(fn):
+    assert os.path.exists(args.datadir), args.target
 
     if args.shards < 2:
         print(f'args.shards is {args.shards}, doing nothing')
         sys.exit()
 
-    corpus = open(args.target).read()
+    corpus = open(f'{args.datadir}/{fn}').read()
     shard_length = int(math.ceil(len(corpus) / args.shards))
 
     offset = 0
     for i in range(args.shards):
-        original_location = os.path.dirname(args.target)
-        new_location = f"{original_location}-{i:05d}-of-{args.shards:05d}"
+        new_location = f"{args.datadir}-{i:05d}-of-{args.shards:05d}"
         if os.path.exists(new_location):
-            print(f"{new_location} exists, deleting")
-            #            os.system(f'rm -Rf {new_location}')
+            pass
+        else:
+            os.system(f'mkdir {new_location}')
 
-        os.system(f'mkdir {new_location}')
         shard = corpus[offset:offset + shard_length]
         offset += shard_length
-        with open(new_location + '/train.txt', 'w') as f:
-            print(f"{new_location}: {len(shard)}")
+        target = f'{new_location}/{fn}'
+        with open(f'{target}', 'w') as f:
+            print(f"{target}: {len(shard)}")
             f.write(shard)
 
 
+def main():
+    shard('train.txt')
+    shard('valid.txt')
+    shard('test.txt')
+    
 if __name__ == '__main__':
   main()
