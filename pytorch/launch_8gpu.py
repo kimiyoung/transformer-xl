3e6d8b0abe7efa2c96b37e74f9b20e8d15ef638e
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-04-24 16:52:07 -0700

    Two machine version
    python launch_8gpu.py --name=txl-2-amazon --machines=2 --instance_type=p3dn.24xlarge --num_rings=10
    
    80k tokens/second on 16 gpus
    
    runs.new/txl-2-amazon.03
    bw around 20Gbps
    step time: 458 (local is 284)

--- pytorch/launch_8gpu.py
+++ pytorch/launch_8gpu.py
@@ -10,60 +10,24 @@ parser.add_argument('--name', type=str, default='txl-1',
 parser.add_argument('--machines', type=int, default=1,
                     help="how many machines to use")
 parser.add_argument('--instance_type', type=str, default="p3.16xlarge",
-                    help="how many machines to use")
+                    help="which instance type to use")
 parser.add_argument('--image_name', type=str,
                     default='Deep Learning AMI (Ubuntu) Version 22.0',
                     help="name of AMI to use ")
-parser.add_argument('--nccl_rings', action='store_true', default=False,
-                    help='use special nccl ring setup')
-parser.add_argument('--nccl_8rings', action='store_true', default=False,
-                    help='use special nccl ring setup')
+parser.add_argument('--num_rings', type=int, default=10,
+                    help='how many rings to use in multimachine setting')
 args = parser.parse_args()
 
 ncluster.set_backend('aws')
 
-
-def get_nccl_rings(num_tasks, num_gpus):
-  ring = build_ring_order(range(num_tasks), range(num_gpus))
-  ring_rev = build_ring_order(reversed(range(num_tasks)),
-                              reversed(range(num_gpus)))
-  rotated_gpu_order = [3, 2, 1, 0, 7, 6, 5, 4]
-  skip_gpu_order = get_skip_order(num_gpus)
-  if (num_tasks >= 4) and (num_gpus == 8):
-    assert ((num_tasks % 4) == 0)
-    skip_machine_order = get_skip_order(num_tasks)
-    ring_skip = build_ring_order(skip_machine_order, rotated_gpu_order)
-    ring_skip_rev = build_ring_order(reversed(skip_machine_order),
-                                     skip_gpu_order)
-    rings_arr = [ring, ring_rev, ring_skip, ring_skip_rev]
-    # rings_arr = [ring, ring_rev, ring_skip]
-  else:
-    rings_arr = [ring, ring_rev]
-  return ' | '.join(rings_arr)
-
-
-def build_ring_order(machine_order, gpu_order):
-  gpu_order = list(gpu_order)
-  machine_order = list(machine_order)
-  ngpus = len(gpu_order)
-  r_order = [(x * ngpus) + y for x in machine_order for y in gpu_order]
-  return ' '.join(map(str, r_order))
-
-
-def get_skip_order(size):
-  if size == 4:
-    return [0, 2, 1, 3]
-  skip_step = 5 if size == 16 else 3
-  # step size of 3 yields - [0,3,6,1,4,7,2,5]
-  return [(i * skip_step) % size for i in range(size)]
-
-
 # routines to build NCCL ring orders
-def get_nccl_params(num_tasks, num_gpus):
-  if num_tasks <= 1 or not args.nccl_rings:
-    return 'NCCL_DEBUG=VERSION'
-  nccl_rings = get_nccl_rings(num_tasks, num_gpus)
-  return f'NCCL_RINGS="{nccl_rings}" NCCL_SINGLE_RING_THRESHOLD=10 NCCL_DEBUG=VERSION'
+def get_nccl_params(num_tasks, _num_gpus):
+    params = f'NCCL_DEBUG=VERSION '
+    # todo(y): try NCCL_SINGLE_RING_THRESHOLD=10, custom ring definition
+    if num_tasks > 1:
+        params += f'NCCL_MIN_NRINGS={args.num_rings} '
+
+    return params
 
 
 def main():
@@ -73,6 +37,7 @@ def main():
                           num_tasks=args.machines,
                           image_name=args.image_name,
                           instance_type=args.instance_type)
+  print(f"Logging to {job.logdir}")
 
   job.upload('*')
   job.run('killall python || echo failed')  # kill previous run
@@ -135,7 +100,6 @@ def main():
     task.run(f'echo {cmd} > {job.logdir}/task-{i}.cmd')  # save command-line
     task.run(cmd, non_blocking=True)
 
-  print(f"Logging to {job.logdir}")
 
 
 if __name__ == '__main__':
