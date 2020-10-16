4485b294c42261cd2dfa55bb1a090b35ae98dda7
Bao HQ <hqbao1991@gmail.com>
2020-10-16 15:26:35 +0800

    Remove unnecessary reshape

--- tf/model.py
+++ tf/model.py
@@ -34,7 +34,6 @@ def rel_shift(x):
   x = tf.pad(x, [[0, 0], [1, 0], [0, 0], [0, 0]])
   x = tf.reshape(x, [x_size[1] + 1, x_size[0], x_size[2], x_size[3]])
   x = tf.slice(x, [1, 0, 0, 0], [-1, -1, -1, -1])
-  x = tf.reshape(x, x_size)
 
   return x
 
